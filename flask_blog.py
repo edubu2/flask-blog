b586865b420ac570ba3a2ecf5ca2994184cb2971
Elliot Wilens <wilensel@gmail.com>
2020-10-28 12:17:13 -0500

    Update script to use templates instead of in-script html

--- flask_blog.py
+++ flask_blog.py
@@ -1,16 +1,16 @@
 #!/Users/ewilens/anaconda3/bin/python3
 
-from flask import Flask # import Flask class
+from flask import Flask, render_template
 app = Flask(__name__) # __name__ is just the name of the current module, helps flask find libraries/static files
 
 @app.route('/') # create home route. Browser will display whatever is returned from the following function
 @app.route('/home') # make it so / and /home go to the same place
 def home_page():
-    return "<h1>Home Page!</h1>"
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
