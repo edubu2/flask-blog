1b5bad6424dd7e83684e24e120e9b110794b9f96
Elliot Wilens <wilensel@gmail.com>
2020-10-28 15:53:36 -0500

    Add titles to / and /about using if statement

--- flask_blog.py
+++ flask_blog.py
@@ -3,14 +3,32 @@
 from flask import Flask, render_template
 app = Flask(__name__) # __name__ is just the name of the current module, helps flask find libraries/static files
 
+# for now let's pretend we made a db call to retrieve this list of posts
+posts = [
+    {
+        'author': 'Elliot Wilens',
+        'title': 'Learning Flask part 1',
+        'content': 'First post content',
+        'date_posted': 'October 27, 2020'
+    },
+    {
+        'author': 'Megan Harpell',
+        'title': "I love Elliot's new site!",
+        'content': "Elliot's site is dope. Super fly, even.",
+        'date_posted': 'October 28, 2020'
+    }
+]
+
 @app.route('/') # create home route. Browser will display whatever is returned from the following function
 @app.route('/home') # make it so / and /home go to the same place
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts) # we can now access this variable in the template using arg name 'posts'
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+print(type(render_template))
