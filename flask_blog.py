34fff34a4820594b768aae54f332ffef5bd39b80
Elliot Wilens <wilensel@gmail.com>
2020-10-28 11:59:14 -0500

    Update flask_blog to run website directly from script

--- flask_blog.py
+++ flask_blog.py
@@ -1,7 +1,11 @@
+#!/Users/ewilens/anaconda3/bin/python3
+
 from flask import Flask # import Flask class
 app = Flask(__name__) # __name__ is just the name of the current module, helps flask find libraries/static files
 
-
-@app.route('/') # create routes. These are what are used by our browser to go to different pages
+@app.route('/') # create home route. Browser will display whatever is returned from the following function
 def hello_world():
-    return 'Hello, World!'
+    return '<h1>Home Page!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
