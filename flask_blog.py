f97a616a2c2e8ee7943258064b8c607baae31170
Elliot Wilens <wilensel@gmail.com>
2020-10-30 10:50:32 -0500

    Add secret key to flask_blog.py

--- flask_blog.py
+++ flask_blog.py
@@ -1,7 +1,9 @@
 #!/Users/ewilens/anaconda3/bin/python3
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_f
 app = Flask(__name__) # __name__ is just the name of the current module, helps flask find libraries/static files
+
+app.config['SECRET_KEY'] = '2c6f7da15b2e9db5e91dd289c8e75a9f' # generated in terminal using secrets module
 
 # for now let's pretend we made a db call to retrieve this list of posts
 posts = [
