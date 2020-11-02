47a072413b5a781ab50ee43cf15cba88afae6fef
Elliot Wilens <wilensel@gmail.com>
2020-11-02 15:32:21 -0600

    Add validations to registration page (via forms.py)
    Will throw an error if email or username is already taken
    
    Tested by trying to recreate an account with my username & email

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_blog.models import User
 
 class RegistrationForm(FlaskForm):
     # username field (requires wtforms StringField, and length validator imports)
@@ -14,6 +15,17 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                             validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    # Custom validations:
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:        # True if user is NOT empty, False otherwise
+            raise ValidationError('Username is already taken. Please choose a different one.')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:        # True if user is NOT empty, False otherwise
+            raise ValidationError('Email address is already taken. Please choose a different email address.')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
