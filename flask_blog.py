e64e3e4a8bba433691228e948b554da4fc8f57c3
Elliot Wilens <wilensel@gmail.com>
2020-11-02 09:39:02 -0600

    Create SQLAlchemy DB ('site.db') in flask_blog.py
    2 tables at this point - 'User' and 'Post'. Not yet interfacing
    with application.
    
    I will soon move the db classes into another module.

--- flask_blog.py
+++ flask_blog.py
@@ -1,10 +1,37 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) # __name__ is just the name of the current module, helps flask find libraries/static files
 
+# Use secret key (created by random in terminal w/ python secrets)
+app.config['SECRET_KEY'] = '2c6f7da15b2e9db5e91dd289c8e75a9f'
 
-app.config['SECRET_KEY'] = '2c6f7da15b2e9db5e91dd289c8e75a9f' # generated in terminal using secrets module
+# Database Config:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return "User({}, {}, {})".format(self.username, self.email, self.image_file)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr(self):
+        return "{}, {}".format(self.title, self.date_posted)
 
 # for now let's pretend we made a db call to retrieve this list of posts
 posts = [
