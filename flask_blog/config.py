a9e88cb921be7bbce1d23407551e42515e09d69a
Elliot Wilens <wilensel@gmail.com>
2020-11-06 23:28:38 +0000

    Adjust config.py to only pull sensitive info from json

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,15 +1,15 @@
 import os
 import json
 
-with open('/etc/config.json') as config_file
+with open('/etc/config.json') as config_file:
 	config = json.load(config_file)
 
 class Config:
     SECRET_KEY = config.get("FB_SECRET")
     SQLALCHEMY_DATABASE_URI = config.get("DB_URI")
     # Configure email
-    MAIL_SERVER = config.get("MAIL_SERVER")
-    MAIL_PORT = config.get("MAIL_PORT")
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = config.get("MAIL_USERNAME")
     MAIL_PASSWORD = config.get("MAIL_PASS")
