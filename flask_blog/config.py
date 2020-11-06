3674cb937bd315db1ba28c969423078cdeb60928
Elliot Wilens <wilensel@gmail.com>
2020-11-06 23:01:56 +0000

    Use /etc/config.json instead of env variables for sensitive info

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,11 +1,15 @@
 import os
+import json
+
+with open('/etc/config.json') as config_file
+	config = json.load(config_file)
 
 class Config:
-    SECRET_KEY = os.environ.get("FB_SECRET")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI")
+    SECRET_KEY = config.get("FB_SECRET")
+    SQLALCHEMY_DATABASE_URI = config.get("DB_URI")
     # Configure email
-    MAIL_SERVER = os.environ.get("MAIL_SERVER")
-    MAIL_PORT = os.environ.get("MAIL_PORT")
+    MAIL_SERVER = config.get("MAIL_SERVER")
+    MAIL_PORT = config.get("MAIL_PORT")
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASS")
+    MAIL_USERNAME = config.get("MAIL_USERNAME")
+    MAIL_PASSWORD = config.get("MAIL_PASS")
