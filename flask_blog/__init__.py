bfe6eaf89aa4f569b4f995967005a88d346c8d2c
Elliot Wilens <wilensel@gmail.com>
2020-11-06 14:59:14 -0600

    Add error handling package for 404, 403, 500 errors

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -29,8 +29,10 @@ def create_app(config_class=Config):
     from flask_blog.users.routes import users
     from flask_blog.posts.routes import posts 
     from flask_blog.main.routes import main
+    from flask_blog.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
