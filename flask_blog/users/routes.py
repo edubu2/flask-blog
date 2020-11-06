8ca336d0e7eb43198fce99a299559775bc2ec096
Elliot Wilens <wilensel@gmail.com>
2020-11-06 12:37:56 -0600

    Update url_for destination from home --> main.home

--- flask_blog/users/routes.py
+++ flask_blog/users/routes.py
@@ -43,7 +43,7 @@ def login():
 @users.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('main.home'))
 
 @users.route('/account', methods=['GET', 'POST'])
 @login_required
