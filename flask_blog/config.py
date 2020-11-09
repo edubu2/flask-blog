1eb12b1e06910349169677037b16810ea6e553ed
Elliot Wilens <wilensel@gmail.com>
2020-11-09 16:38:16 +0000

    Update config file for server (email not working)

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,11 +1,16 @@
 import os
+import json
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
+
 
 class Config:
-    SECRET_KEY = os.environ.get("FB_SECRET")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI")
+    SECRET_KEY = config.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
     # Configure email
-    MAIL_SERVER = os.environ.get("MAIL_SERVER")
-    MAIL_PORT = os.environ.get("MAIL_PORT")
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASS")
+    MAIL_USERNAME = config.get("EMAIL_USER")
+    MAIL_PASSWORD = config.get("EMAIL_PASS")
