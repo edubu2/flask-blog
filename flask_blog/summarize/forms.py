69a5fb2e2ee413b951dfad0745980cd57ec2a962
Elliot Wilens <wilensel@gmail.com>
2020-11-13 16:12:42 -0600

    Add summarize forms.py and routes.py (not working properly at this point)

--- flask_blog/summarize/forms.py
+++ flask_blog/summarize/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 from wtforms import SubmitField, TextAreaField
 
 
 class SummarizeForm(FlaskForm):
-    full_text = TextAreaField('Long text', validators=[DataRequired])
+    full_text = TextAreaField('Long text', validators=[DataRequired()])
     summary = TextAreaField('Summary')
     submit = SubmitField('Summarize')
+
+    def validate_text_length(self, full_text):
+        """Ensures the user's text input is long enough to generate a strong summary. """
+
+        if len(full_text) < 1000:
+            raise ValidationError(
+                "Text is not long enough to generate a summary.")
--- flask_blog/summarize/routes.py
+++ flask_blog/summarize/routes.py
@@ -1,15 +1,18 @@
 from flask import render_template, flash, Blueprint
 from flask_blog.summarize.forms import SummarizeForm
+from flask_blog.summarize.utils import summarize_text
 
 summarize = Blueprint('summarize', __name__)
 
 
 @summarize.route('/summarize-text', methods=['GET', 'POST'])
-def summarize_text():
-    """ This route will hold the NLP Text Summarize module. """
+def magic_summarize():
+    """ This route allows users to get a summarized version of any long text (such as an article or email). """
+
     form = SummarizeForm()
     if form.validate_on_submit():
         full_text = form.full_text.data
-        summarize_text =
+        summary = summarize_text(full_text)
+        form.summary.data = summary
 
-    return render_template('summarize.html', form=form title='Summarize Text')
+    return render_template('summarize.html', form=form, title='Summarize Text', summary=summary)
