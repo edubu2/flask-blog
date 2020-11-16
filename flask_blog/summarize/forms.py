f382b6c09fd4731dcf0bdab6583c4bf9f1f9d11a
Elliot Wilens <wilensel@gmail.com>
2020-11-16 11:14:38 -0600

    Increase height of summary field in Paraphrase tab of site

--- flask_blog/summarize/forms.py
+++ flask_blog/summarize/forms.py
@@ -5,7 +5,7 @@ from wtforms import SubmitField, TextAreaField
 
 class SummarizeForm(FlaskForm):
     full_text = TextAreaField('Long text', validators=[DataRequired()])
-    summary = TextAreaField('Summary')
+    summary = TextAreaField('Summary', render_kw={"cols":35, "rows":12})
     submit = SubmitField('Summarize')
 
     def validate_text_length(self, full_text):
